d910fc8c43b383846b26429e329deda3c1734b38
Andrew Chai <andrewchai@Andrews-Mac-mini.local>
2018-04-16 17:56:52 +0800

    update

---
+++ ExportNsLayout/ExportNsLayout/AdaptiveViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdaptiveViewController.swift
+//  ExportNsLayout
+//
+//  Created by Andrew Chai on 4/16/18.
+//  Copyright © 2018 Andrew Chai. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AdaptiveViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillLayoutSubviews() {
+    
+    }
+    
+    
+}

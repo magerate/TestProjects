46816015da6718f9e7f1ac7c299d04f282714f23
Andrew Chai <andrewchai@Andrews-Mac-mini.local>
2018-04-25 10:07:22 +0800

    observer

--- ExploreRunLoop/ExploreRunLoop/ViewController.swift
+++ ExploreRunLoop/ExploreRunLoop/ViewController.swift
@@ -8,6 +8,30 @@
 
 import UIKit
 
+extension CFRunLoopActivity  : CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        var activityValue = ""
+        if self == CFRunLoopActivity.entry {
+            activityValue = "entry"
+        } else if self == CFRunLoopActivity.beforeTimers {
+            activityValue = "beforeTimers"
+        } else if self == CFRunLoopActivity.beforeSources {
+            activityValue = "beforeSources"
+        } else if self == CFRunLoopActivity.beforeWaiting {
+            activityValue = "beforeWaiting"
+        } else if self == CFRunLoopActivity.afterWaiting {
+            activityValue = "afterWaiting"
+        } else if self == CFRunLoopActivity.exit {
+            activityValue = "exit"
+        }
+        
+        return "\(activityValue) rawValue = \(self.rawValue) mode = \(RunLoop.current.currentMode!)"
+    }
+    
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -18,6 +42,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupRunLoopObserver() {
+        let myRunLoop = RunLoop.current
+        
+        let observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault,
+                                                          CFRunLoopActivity.allActivities.rawValue ,
+                                                          true, 0,
+                                                          runLoopHandler)
+        
+        CFRunLoopAddObserver(myRunLoop.getCFRunLoop(), observer, CFRunLoopMode.commonModes)
+        
+    }
+    
+    func runLoopHandler(observer: CFRunLoopObserver?, activity: CFRunLoopActivity) {
+        print(activity)
     }
 
 
